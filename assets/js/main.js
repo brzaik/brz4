8a3d54e54691279c449bf9aa5e89b52e1322b048
Brian R Zaik <brian.r.zaik@gmail.com>
2017-06-25 13:47:16 -0700

    Built out the rest of Home

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,23 @@ $(document).ready(function(){
     lazyLoad: 'progressive',
     infinite: false,
     initialSlide: $('.slick-cards > div').size() - 1,
+    nextArrow:"<div class='a-right control-c next slick-next'><img src='/assets/images/slick_arrow_right.png' /></div>",
+    prevArrow:"<div class='a-left control-c prev slick-prev'><img src='/assets/images/slick_arrow_left.png' /></div>",
+    swipe: true,
+    swipeToSlide: true,
+    touchMove: true,
+    variableWidth: true
+  });
+
+  $('.slick-photog').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    centerMode: true,
+    dots: false,
+    infinite: true,
+    lazyLoad: 'progressive',
+    speed: 5000,
     variableWidth: true
   });
 
