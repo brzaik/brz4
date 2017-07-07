3d31e21f01db65cb562c7b58eea033f9689f1327
zaik <zaik@coupang.com>
2017-07-07 00:21:44 -0700

    Added About page and lots of work content

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,37 @@ $(document).ready(function(){
     variableWidth: true
   });
 
+  $('.slick').slick({
+    lazyLoad: 'progressive',
+    infinite: false,
+    nextArrow:"<div class='a-right control-c next slick-next'><img src='/assets/images/slick_arrow_right.png' /></div>",
+    prevArrow:"<div class='a-left control-c prev slick-prev'><img src='/assets/images/slick_arrow_left.png' /></div>",
+    swipe: true,
+    swipeToSlide: true,
+    touchMove: true,
+    adaptiveHeight: true,
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+  });
+
   // JavaScript
   window.sr = ScrollReveal();
     sr.reveal('.card-work', {});
