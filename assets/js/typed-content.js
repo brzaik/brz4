e463696506bf80ef721bb356759425bf25f61af6
Brian R Zaik <brian.r.zaik@gmail.com>
2021-11-14 17:13:53 -0800

    Updated wording.

--- assets/js/typed-content.js
+++ assets/js/typed-content.js
@@ -1,13 +1,13 @@
 var typed = new Typed('header span.typed', {
   strings: [
-    "a product leader based in San Francisco.",
+    "a product manager and designer based in San Francisco.",
     "much more than can be described in one sentence.",
     "a creative with a coding problem.",
     "lover of soul and Motown (especially Marvin and Aretha).",
     "and it took me two months to figure out what to put here.",
     "ready to bring order to chaos.",
     "prone to diving out of perfectly good airplanes.",
-    "a product leader based in San Francisco."
+    "a product manager and designer based in San Francisco."
   ],
   typeSpeed: 10,
   backSpeed: 10,
